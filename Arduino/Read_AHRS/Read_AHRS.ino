c59e8e21622aa88997a6e90e0335a9d9f73649ab
Sean Guo <iguoxiaopeng@gmail.com>
2014-04-12 17:58:14 +0800

    串口

--- Arduino/Read_AHRS/Read_AHRS.ino
+++ Arduino/Read_AHRS/Read_AHRS.ino
@@ -2,8 +2,9 @@
   Serial1 - /dev/ttyS0   in Galileo - AHRS CJMCU
   Serial  - /dev/ttyGS0  in Galileo - Arduion IDE Serial Monitor
   
+  
+  AHRS must connect the VCC5V with Galileo!
 */
-
 void setup() {
   // put your setup code here, to run once:
   Serial1.begin(57600);
@@ -16,8 +17,14 @@ void loop() {
   // check if data has been sent from AHRS:
   if (Serial1.available()) {
     // read the most recent byte (which will be from 0 to 255):
-    read_data = char(Serial1.read());
+    read_data = Serial1.read();
     Serial.print(read_data);  // print to Serial(/dev/ttyGS0)
+  }
+  
+  if (Serial.available()) {
+    // read the most recent byte (which will be from 0 to 255):
+    read_data = Serial.read();
+    Serial1.print(read_data);  // print to Serial(/dev/ttyGS0)
   }
   
   
