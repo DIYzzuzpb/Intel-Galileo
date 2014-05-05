ae04d8fa95524284b189bb7f72b02404070af3f0
Sean Guo <iguoxiaopeng@gmail.com>
2014-05-05 10:59:31 +0800

    PWM for three motor

---
+++ Arduino/PWM_3motor/PWM_3motor.ino
@@ -0,0 +1,86 @@
+#include "Wire.h"
+
+// PWM pin : 3,5,6,9,10,11
+
+int pwm1 = 3;
+int pwm2 = 6;
+int pwm3 = 9;
+
+int dir1 = 2;
+int dir2 = 5;
+int dir3 = 8;
+
+void setup() {
+  Wire.begin();
+  setPwmI2C (pwm1, 100);
+  setPwmI2C (pwm2, 200);
+  setPwmI2C (pwm3, 127);
+ 
+  pinMode(dir1, OUTPUT);
+  pinMode(dir2, OUTPUT);
+  pinMode(dir3, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(dir1, HIGH);
+  digitalWrite(dir2, HIGH);
+  digitalWrite(dir3, HIGH);
+  
+  delay(3000); //delay 3s
+  
+  digitalWrite(dir1, LOW);
+  digitalWrite(dir2, LOW);
+  digitalWrite(dir3, LOW);
+  
+  delay(3000); //delay 3s
+}
+
+//set PWM use I2C 
+//set pin(_iPinNum) output PWM with duty cycle of (_iPwmVal / 255).
+void setPwmI2C(int _iPinNum, int _iPwmVal)
+{
+  // Select pin to write I2C commands to
+  analogWrite(_iPinNum,1);            //analogWrite(pin, 1) will enable the PWM on the pin. 
+                                      //but AnalogWrite(pin,0) does not disable the pwm.
+
+  //Then there’s the I2C commands...
+
+
+  // Select programmable PWM CLK source
+  Wire.beginTransmission(0x20);
+  Wire.write(0x29);  // CY8C9540A -  Config (29h)
+  //xxxxx000b - 32kHz (default)
+  //xxxxx001b - 24Mhz
+  //xxxxx010b - 1.5Mhz
+  //xxxxx011b - 93.75 kHz
+  //xxxxx100b - 367.6 Hz (programmable)
+  //xxxxx101b - Previous PWM
+  //Wire.write(0x04);  // xxxxx100b - 367.6 Hz (programmable)
+  Wire.write(0x02);    // 1.5MHZ , but output just 6khz. just use it~
+  Wire.endTransmission();
+  
+  // Set divider the PWM CLK .
+  Wire.beginTransmission(0x20);
+  Wire.write(0x2C);
+  Wire.write(0x01);
+  Wire.endTransmission();
+
+  // Set period register (2Ah) - 0x01 / 0xff
+  Wire.beginTransmission(0x20);
+  Wire.write(0x2a);
+  //xxxx0xxxb - Falling pulse edge (default)      
+  //xxxx1xxxb - Rising pulse edge
+  Wire.write(0xff);
+  Wire.endTransmission();
+
+  // Set minimum duty cycle - Pulse Width Register (2Bh)
+  Wire.beginTransmission(0x20);
+  Wire.write(0x2b);
+  //This register sets the pulse width of the PWM output. 
+  //Allowed values are between zero and the (Period - 1) value.
+  ////// this is the pulse width...0-255 where 255 is all "on" for one cycle (1/clock frequency)
+  Wire.write(_iPwmVal);
+  Wire.endTransmission();
+  
+}
