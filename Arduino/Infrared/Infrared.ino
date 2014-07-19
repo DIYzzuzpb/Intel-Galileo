3b6b21d8aff5dc8d0ef7cbe9fb90e0a8b5ac3779
Sean Guo <iguoxiaopeng@gmail.com>
2014-07-20 07:03:25 +0800

    little update about infrared & pwm_3motor

--- Arduino/Infrared/Infrared.ino
+++ Arduino/Infrared/Infrared.ino
@@ -33,28 +33,62 @@ int sensorValue3 = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
   sensorValue1 = analogRead(pin_M1); //6ms
-  sensorValue2 = analogRead(pin_M2); //6ms
+  //sensorValue2 = analogRead(pin_M2); //6ms
   //AD, 0V -> 0, 5V -> 1024, voltage = sensorValue * 5 / 1024 = sensorValue / 204.8;
   
   Serial.print(sensorValue1 / 204.8);
   Serial.print(" ");
-  Serial.println(sensorValue2 / 204.8);
-  //Serial.println(get_distance(sensorValue1 / 204.8));
-}
-
-double get_distance(double voltage)
-{
-  // 0 ~ 10cm
-  return (voltage / 2.4);
+  Serial.println(get_distance(sensorValue1 / 204.8));
 }
 
 
-void detection()
+int get_distance(double voltage)
 {
+  // 1s motor yi shang.
   
+  /*
+  10      2.59 -
+  11      2.56 - 2.59
+  12      2.55 - 2.56
+  13      2.53 - 2.54
+  14      2.52 - 2.53
+  15      2.51 - 2.52
+  16      2.49 - 2.50 
+  17      2.48 - 2.49
+  18      2.45 - 2.48 -----
+  19      2.43 - 2.44
+  20      2.42 - 2.43
+  21      2.38 - 2.42
+  22      2.37 - 2.38
+  23      2.33 - 2.37
+  24      2.33 - 2.37
+  25      2.30 - 2.33
+  26      2.27 - 2.30
+  27      2.24 - 2.27
+  27      2.20 - 2.24  
+  */
+  
+  if(voltage > 2.59) return 10;
+  else if(voltage > 2.56) return 11;
+  else if(voltage > 2.55) return 12;
+  else if(voltage > 2.53) return 13;
+  else if(voltage > 2.52) return 14;
+  else if(voltage > 2.51) return 15;
+  else if(voltage > 2.49) return 16;
+  else if(voltage > 2.48) return 17;
+  else if(voltage > 2.45) return 18;
+  else if(voltage > 2.43) return 19;
+  else if(voltage > 2.42) return 20;
+  else if(voltage > 2.38) return 21;
+  else if(voltage > 2.37) return 22;
+  else if(voltage > 2.34) return 23;
+  else if(voltage > 2.33) return 24;
+  else if(voltage > 2.30) return 25;
+  else if(voltage > 2.27) return 26;
+  return 27;
 }
